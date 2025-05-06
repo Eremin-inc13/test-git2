3c95be77aa09df159f62fe6c42b6f5651d52fed4
Alex Eremin <eremin_inc@mail.ru>
2025-05-06 16:32:33 +0300

    ololo

--- index.js
+++ index.js
@@ -2,13 +2,6 @@ console.log(`Привет Мир`);
 
 console.error(`Ошибка`);
 
-const userAge = 35;
-
-if (userAge >= 18) {
-  console.log(`топ`);
-} else {
-  console.log(`не топ`);
-}
 const age = 20;
 if (age >= 18) {
   console.log(`Вы совершеннолетний`);
