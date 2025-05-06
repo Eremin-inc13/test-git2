80d5b254dcb85471deca5b114405e4962cae0b86
Alex Eremin <eremin_inc@mail.ru>
2025-05-06 16:26:09 +0300

    made changes in feature/user

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 console.log(`Привет Мир`);
 
 console.error(`Ошибка`);
+
+const age = 20;
+if (age >= 18) {
+  console.log(`Вы совершеннолетний`);
+} else {
+  console.log(`Вы не совершеннолетний`);
+}
